ca19d18ed0bfa07950a6576af9c114f11e845a23
Николай <nikolayivanov944@gmail.com>
2024-11-03 13:42:05 +0300

    Init

--- src/ServiceStation.java
+++ src/ServiceStation.java
@@ -1,9 +1,5 @@
 public class ServiceStation {
-    public void check(Serviceable... transports) {
-        for (Serviceable transport : transports) {
+    public void check(Serviceable transport, Car car, Truck truck) {
             transport.repair();
-        }
-
-
     }
 }
